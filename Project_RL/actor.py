f43ef56a96aa4ec9355e76e6c68f6f32777d2c17
kroegepa <paulkroeger98@gmail.com>
2025-01-13 17:00:02 +0100

    sth

--- Project_RL/actor.py
+++ Project_RL/actor.py
@@ -8,3 +8,37 @@ class Actor():
 class UniformBuyActor(Actor):
     def act(self, state) -> float:
         return 0.5
+
+class AveragedBuyingActor(Actor):
+    def __init__(self, amount_of_prices: int = 120, threshold: float = 0.1):
+        self.current_average : float = 0
+        self.price_queue = []
+        #Amount of prices to be considered
+        #24 prices to a day
+        self.amount_of_prices = amount_of_prices
+        #Deviation from average price to trigger buy sell action
+        self.threshold = threshold
+    def updateAverage(self, newPrice:int):
+        if len(self.price_queue) == self.amount_of_prices:
+            self.current_average -= self.price_queue[0]/self.amount_of_prices
+            self.current_average += newPrice/self.amount_of_prices
+            self.price_queue.pop(0)
+            self.price_queue.append(newPrice)
+        else:
+            self.price_queue.append(newPrice)
+            self.current_average = sum(self.price_queue)/len(self.price_queue)
+    def makeDecision(self, current_price:int) -> float:
+        if self.current_average == 0:
+            return 1
+        difference = current_price - self.current_average
+        fraction  = difference/self.current_average
+        #TODO maybe make sure that
+        if fraction > self.threshold:
+            return -1
+        if fraction * -1 > self.threshold:
+            return 1
+
+        return 0
+        
+        
+
--- Project_RL/main.py
+++ Project_RL/main.py
@@ -15,13 +15,16 @@ environment = DataCenterEnv(path_to_dataset)
 aggregate_reward = 0
 terminated = False
 state = environment.observation()
-agent = actor.UniformBuyActor()
+agent = actor.AveragedBuyingActor()
 while not terminated:
     # agent is your own imported agent class
-    action = agent.act(state)
+    action = agent.makeDecision(state[1])
+    #agent.updateAverage(state[1])
+    print(action)
     #action = np.random.uniform(-1, 1)
     # next_state is given as: [storage_level, price, hour, day]
     next_state, reward, terminated = environment.step(action)
+    
     state = next_state
     aggregate_reward += reward
     print("Action:", action)
