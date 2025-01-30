578caaa96d9091f6d5bba8e6d865312df32700cf
TheGrimMumble <thom.grims@gmail.com>
2025-01-30 18:24:05 +0100

    cleaning up

--- Project_RL/diagnostic_plots.py
+++ Project_RL/diagnostic_plots.py
@@ -2,7 +2,7 @@ import actor
 import numpy as np
 from env import DataCenterEnv
 
-addon = "test_p0.1_w5_g0.9_a0.01_r1_"
+addon = ""
 
 trajectory_path = "tab_q_val_trajectory.csv"
 validation_reward_path = "tab_q_val_rewards.csv"
@@ -17,7 +17,7 @@ environment_test = DataCenterEnv('validate.xlsx')
 agent = actor.TabularQActor(environment_train, environment_test)
 agent.Q = Qs
 
-agent.visualize_trajectory(trajectory_data, 300, 330, nmbr_days_in_fig=5, spam_protection=False)
+agent.visualize_trajectory(trajectory_data, 724, None, nmbr_days_in_fig=5, spam_protection=True)
 agent.plot_rewards_during_training(validation_reward_data)
 agent.visualize_q_values('storage')
 agent.visualize_q_values('action')
